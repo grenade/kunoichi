3d83aeeac0c2f384dd3800b8092667abf036f410
Rob Thijssen <rthijssen@gmail.com>
2017-05-17 19:15:15 +0300

    service run state monitor

--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,19 @@ fn main() {
   let daemon = Daemon {
     name: "kunoichi".to_string()
   };
+  let mut running = false;
   daemon.run(move |rx: Receiver<State>| {
     for signal in rx.iter() {
       match signal {
         State::Start => {
-          service_start();
+          service_start(&mut running);
         },
         State::Reload => {
-          service_stop();
-          service_start();
+          service_stop(&mut running);
+          service_start(&mut running);
         },
         State::Stop => {
-          service_stop();
+          service_stop(&mut running);
         }
       };
     }
@@ -30,15 +31,28 @@ fn main() {
   log("application terminating.");
 }
 
-fn service_start() {
-  log("service starting...");
-  // todo: start kunoich if not running
-  log("service started.");
+fn service_start(running: &mut bool) {
+  match running {
+    &mut true => log("service is already started."),
+    &mut false => {
+      log("service starting...");
+      *running = true;
+      // todo: start service and make a mess
+      log("service started.");
+    },
+  }
 }
-fn service_stop() {
-  log("service stopping...");
-  // todo: stop kunoich if running
-  log("service stopped.");
+
+fn service_stop(running: &mut bool) {
+  match running {
+    &mut true => {
+      log("service stopping...");
+      *running = false;
+      // todo: stop service and clean up
+      log("service stopped.");
+    },
+    &mut false => log("service is already stopped."),
+  }
 }
 
 #[allow(unused_must_use)]
@@ -48,6 +62,7 @@ fn log(message: &str) {
 
 fn log_safe(message: &str) -> Result<(), Error> {
   println! ("{}", message);
+  // todo: either add timestamp or (preferred) lose file in favour of eventlog(windows)/syslog(linux)
   let path = try! (env::current_exe()).with_extension("log");
   let mut file = try! (OpenOptions::new().create(true).write(true).append(true).open(&path));
   try! (file.write(message.as_bytes()));
